3870db2318e1604a517f0459ebee5f47399837ef
Jithin Sebastian <12930413+jithinsebastian-git@users.noreply.github.com>
2025-12-19 12:59:45 +0530

    Modify vue-router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../components/Home.vue'
 import Projects from '../components/Projects.vue'
 import Experience from '../components/Experience.vue'
@@ -10,7 +10,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
